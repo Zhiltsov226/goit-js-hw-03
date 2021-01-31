cf63684dbb98309c947e832691fe90a055e4c32e
Zhiltsov226 <68431279+Zhiltsov226@users.noreply.github.com>
2021-01-31 13:02:49 +0200

    work-1

---
+++ task-2.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const countProps = function (obj) {
+  const values = Object.values(obj);
+  return values.length;
+};
+
+console.log(countProps({}));
+
+console.log(countProps({ name: "Mango", age: 2 }));
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
